2d7e338a5fa91711182a447b049dc60fb8053375
lsy3709 <lsy3709@naver.com>
2026-01-13 17:07:51 +0900

    8교시, 실습3 번 풀이

--- src/_1_test_260113/ex/Ex3.java
+++ src/_1_test_260113/ex/Ex3.java
@@ -1,5 +1,7 @@
 package _1_test_260113.ex;
 
+import java.util.Scanner;
+
 public class Ex3 {
     public static void main(String[] args) {
 //        1)
@@ -9,11 +11,18 @@ public class Ex3 {
 //        성인입니까? true/false
 
         // 스캐너 도구 이용
-
+        Scanner scanner = new Scanner(System.in);
         // 입력 받기
+        System.out.println("나이를 입력해주세요 : ");
         // 스캐너 이용해서 가져오기
+        int age = scanner.nextInt();
 
         // 출력하기.
+        System.out.println("성인입니까? " + (age >= 19));
+
+        // 자원 반납하기.
+//        scanner.close();
+
 //=================================================
 //        삼항 연산자 이용, (조건식) ? 참 : 거짓
 //        점수가 60점 이상이면 "합격" 아니면 "불합격"
@@ -21,11 +30,15 @@ public class Ex3 {
 //        출력 :
 //        합격/불합격 입니다.
         // 스캐너 도구 이용
-
+        // Scanner scanner2 = new Scanner(System.in);
+        // 오류 발생, 기존 스캐너 재사용함.
         // 입력 받기
+        System.out.println("점수를 입력해주세요 : ");
         // 스캐너 이용해서 가져오기
-
+        int score = scanner.nextInt();
         // 출력하기.
-
+        System.out.println((score >= 60) ? "합격입니다." : "불합격입니다.");
+        // 자원 반납하기.
+        scanner.close();
     }
 }
